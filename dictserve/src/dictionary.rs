c51159e20306e4cc922f5ddd1723bf9bb2b6e131
Alex-Programs <alexcj2802@gmail.com>
2025-06-29 14:17:23 +0100

    bad idea

--- dictserve/src/dictionary.rs
+++ dictserve/src/dictionary.rs
@@ -39,7 +39,6 @@ impl DictionaryStore {
 
         // Use rayon to parallelize insertion
         elements.into_par_iter().for_each(|element| {
-            info!("Example entry: {:?}", element);
             store.insert((element.lang.clone(), element.key.clone()), element);
         });
 
